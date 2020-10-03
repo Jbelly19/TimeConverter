5ad2eb80a8dad2a2352cd97d80c35393437edbc4
Josh Belmont <jbelmont19@live.com>
2020-10-03 10:52:21 -0700

    Save, trying to get time picker working and asked for help

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    struct TimeUnits {
+        var unitName: String
+        var unit: UnitDuration
+    }
+    
+    let timeUnits = [TimeUnits(unitName: "Seconds", unit: UnitDuration.seconds), TimeUnits(unitName: "Minutes", unit: UnitDuration.minutes), TimeUnits(unitName: "Hours", unit: UnitDuration.hours)]
+    
+    @State private var inputTime = Date()
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    //                    TextField("Input Time", text: $inputTime)
+                    DatePicker("Input time", selection: $inputTime, displayedComponents: .hourAndMinute)
+                }
+                Section(header: Text("Select Output Units:")){
+                    Picker("Input Unit", selection: $inputUnit){
+                        ForEach(0 ..< timeUnits.count){
+                            Text("\(self.timeUnits[$0].unitName)")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }.textCase(nil)
+            }
+        }.navigationBarTitle("Time Converter")
     }
 }
 
